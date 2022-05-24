14ac5aec4a4df4ad20a2c7471daf5fc2514f6767
Sergey Borozdin <borozdinsm@yandex.ru>
2022-05-24 09:45:21 +0300

    second commit added time slice

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,19 @@ public class Main {
 
         String folderPart = "C:\\Users\\60034452\\Desktop";
         File file = new File(folderPart);
+        long start = System.currentTimeMillis();
         System.out.println(getFolderSize(file));
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
 
     public static long getFolderSize(File folder){
-        // если файл сразу возвращаем длину
         if (folder.isFile()){
             return folder.length();
         }
-
         long sum = 0;
         File[] files = folder.listFiles();
         for (File file : files){
-            // вызывает размер у корневой папки, потом у всех дочерних
-            // и после ссумирует размер всех папок и файлов
             sum += getFolderSize(file);
         }
         return sum;
