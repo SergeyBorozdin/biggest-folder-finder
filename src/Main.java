2a6ff521b8420376b1d176106204b578c12accc8
Sergey Borozdin <borozdinsm@yandex.ru>
2022-05-30 11:05:03 +0300

    four commit added class Node

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
+    private static char[] sizeMultipliers = {'B', 'K', 'M', 'G', 'T'};
+
     public static void main(String[] args) {
+
+//        System.out.println(getHumanReadableSize(240640));
+//        System.exit(0);
+//        System.out.println(getSizeFromHumanReadable("235K"));
+//        System.exit(0);
+//
+//
+
 
         String folderPart = "C:\\Users\\60034452\\Desktop";
         File file = new File(folderPart);
@@ -20,6 +31,40 @@ public class Main {
 
         long duration = System.currentTimeMillis() - start;
         System.out.println(duration + " ms");
+
+    }
+
+    // TODO 24B, 234Kb, 36Mb, 34Gb, 42Tb
+
+    public static String getHumanReadableSize(long size){
+
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+
+            double value = size / Math.pow(1024, i);
+            if (value < 1024) {
+                return Math.round(value) + "" + sizeMultipliers[i] + (i > 0 ? "b" : "");
+            }
+        }
+        return "Недопустимое значение";
+    }
+
+    // TODO 24B, 234Kb, 36Mb, 34Gb, 42Tb
+    public static long getSizeFromHumanReadable(String size){
+
+        HashMap<Character, Integer> char2multiplier = getMutipliers();
+        char sizeFactor = size.replaceAll("[0-9\\s+]", "").charAt(0);
+        int multiplier = char2multiplier.get(sizeFactor);
+        long length = multiplier * Long.valueOf(size.replaceAll("[^0-9]", ""));
+        return length;
+    }
+
+    private static HashMap<Character, Integer> getMutipliers(){
+
+        HashMap<Character, Integer> char2multiplier = new HashMap<>();
+        for (int i = 0; i < sizeMultipliers.length; i++ ){
+            char2multiplier.put(sizeMultipliers[i], (int) Math.pow(1024, i));
+        }
+        return char2multiplier;
     }
 
     public static long getFolderSize(File folder){
